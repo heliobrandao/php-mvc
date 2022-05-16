5e72100fd67f39735c0c214d805c4a1baffeaa0c
heliobrandao <heliob79@gmail.com>
2022-05-16 20:34:04 -0300

    lesson-04

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,18 @@
 <?php
 
-use Alura\Cursos\Controller\FormularioInsercao;
-use Alura\Cursos\Controller\ListarCursos;
-use Alura\Cursos\Controller\Persistencia;
+use Alura\Cursos\Controller\{
+  FormularioInsercao,
+  ListarCursos,
+  Persistencia,
+  Exclusao,
+  FormularioEdicao
+};
 
 return [
   '/listar-cursos' => ListarCursos::class,
   '/novo-curso' => FormularioInsercao::class,
-  '/salvar-curso' => Persistencia::class
+  '/salvar-curso' => Persistencia::class,
+  '/excluir-curso' => Exclusao::class,
+  '/atualizar-curso' => FormularioEdicao::class
+
 ];
