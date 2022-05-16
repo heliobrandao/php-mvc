9b410a51d8a6570c7f2628343fdc9fddf4b26cba
heliobrandao <heliob79@gmail.com>
2022-05-15 21:44:21 -0300

    lesson-03

--- view/cursos/formulario.php
+++ view/cursos/formulario.php
@@ -1,6 +1,6 @@
-<?php include __DIR__ . '/../inicio-html.php'; ?>
+<?php require __DIR__ . '/../inicio-html.php'; ?>
 
-    <form>
+    <form action="/salvar-curso" method="post">
         <div class="form-group">
             <label for="descricao">Descrição</label>
             <input type="text" id="descricao" name="descricao" class="form-control">
@@ -8,4 +8,5 @@
         <button class="btn btn-primary">Salvar</button>
     </form>
 
-<?php include __DIR__ . '/../fim-html.php'; ?>
+
+<?php require __DIR__ . '/../fim-html.php'; ?>
